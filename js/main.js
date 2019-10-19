e2f945959a3e8e211f00b24c465dba9456f35cd5
ANDREYDEN <andrey2850@gmail.com>
2019-10-19 12:26:32 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+    var wwd = new WorldWind.WorldWindow("canvasOne")
+    wwd.addLayer(new WorldWind.BMNGLandsatLayer())
+    wwd.addLayer(new WorldWind.BMNGOneImageLayer())
+
+    var placemarkLayer = new WorldWind.RenderableLayer("Placemark")
+    wwd.addLayer(placemarkLayer)
+
+    var placemarkAttributes = new WorldWind.PlacemarkAttributes(null)
+    placemarkAttributes.imageOffset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION,
+        0.3,
+        WorldWind.OFFSET_FRACTION,
+        0.0
+    )
+
+    placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW
+    placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION,
+        0.5,
+        WorldWind.OFFSET_FRACTION,
+        1.0
+    )
+
+    placemarkAttributes.imageSource =
+        WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png"
+    var position = new WorldWind.Position(55.0, -106.0, 1000000.0)
+    var placemark = new WorldWind.Placemark(
+        position,
+        false,
+        placemarkAttributes
+    )
+
+    placemark.label =
+        "Placemark\n" +
+        "Lat " +
+        placemark.position.latitude.toPrecision(4).toString() +
+        "\n" +
+        "Lon " +
+        placemark.position.longitude.toPrecision(5).toString()
+
+    placemark.alwaysOnTop = true
+    placemarkLayer.addRenderable(placemark)
+    var modelLayer = new WorldWind.RenderableLayer("Duck")
+    wwd.addLayer(modelLayer)
+}
