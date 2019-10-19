fa6870af63361ebcedecaff6ffe0d4b2734aaf1b
ANDREYDEN <andrey2850@gmail.com>
2019-10-19 14:57:48 -0400

    Added movement on drug

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
-window.onload = () => {
-    var wwd = new WorldWind.WorldWindow("canvasOne")
+const initGlobe = () => {
+    let wwd = new WorldWind.WorldWindow("canvasOne")
     wwd.addLayer(new WorldWind.BMNGLandsatLayer())
     wwd.addLayer(new WorldWind.BMNGOneImageLayer())
+    return wwd
+}
 
-    var placemarkLayer = new WorldWind.RenderableLayer("Placemark")
-    wwd.addLayer(placemarkLayer)
-
-    var placemarkAttributes = new WorldWind.PlacemarkAttributes(null)
+const addMarker = ({lat, lng, alt}) => {
+    let placemarkAttributes = new WorldWind.PlacemarkAttributes(null)
     placemarkAttributes.imageOffset = new WorldWind.Offset(
-        WorldWind.OFFSET_FRACTION,
-        0.3,
-        WorldWind.OFFSET_FRACTION,
-        0.0
+        WorldWind.OFFSET_FRACTION, 0.3,
+        WorldWind.OFFSET_FRACTION, 0.0
     )
 
     placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW
     placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-        WorldWind.OFFSET_FRACTION,
-        0.5,
-        WorldWind.OFFSET_FRACTION,
-        1.0
+        WorldWind.OFFSET_FRACTION, 0.5,
+        WorldWind.OFFSET_FRACTION, 1.0
     )
 
     placemarkAttributes.imageSource = "../img/fire.png"
     placemarkAttributes.imageScale = 0.3
 
-    var position = new WorldWind.Position(55.0, -106.0, 1000000.0)
-    var placemark = new WorldWind.Placemark(position, false, placemarkAttributes)
-    placemark.label = '(' + placemark.position.latitude.toPrecision(5).toString() + ', ' + 
-                            placemark.position.longitude.toPrecision(5).toString() + ', ' + 
-                            placemark.position.altitude.toPrecision(10).toString() + ')'
+    let position = new WorldWind.Position(lat, lng, alt)
+    let placemark = new WorldWind.Placemark(
+        position,
+        false,
+        placemarkAttributes
+    )
+    placemark.label =
+        "(" +
+        placemark.position.latitude.toPrecision(5).toString() +
+        ", " +
+        placemark.position.longitude.toPrecision(5).toString() +
+        ", " +
+        placemark.position.altitude.toPrecision(10).toString() +
+        ")"
     placemark.alwaysOnTop = true
-    placemarkLayer.addRenderable(placemark)
-    var modelLayer = new WorldWind.RenderableLayer("Duck")
-    wwd.addLayer(modelLayer)
+    return placemark
 }
+
+const updateMarker = ({ placemark, lat, lng, alt }) => {
+
+}
+
+window.onload = () => {
+    let wwd = initGlobe()
+    var placemarkLayer = new WorldWind.RenderableLayer("Placemark")
+    wwd.addLayer(placemarkLayer)
+    var lng = -106;
+    var lat = 55;
+    placemarkLayer.addRenderable(addMarker({ lat: lat, lng: lng, alt: 1000000 }))
+    setInterval(() => {
+        lng += (0.1 * Math.floor(Math.random() * 5))
+        lat += (0.1 * Math.floor(Math.random() * 5))
+        placemarkLayer.renderables[0].position = new WorldWind.Position(lat, lng, 1000000)
+    }, 10);
+        
+}   
