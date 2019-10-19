329c7808acb82cc592835004bf4f05ada4cb9f07
nmyshuniaiev <nmyshuniaiev@gmail.com>
2019-10-19 17:51:05 -0400

    Linked satellite position to marker

--- emojillite/views.py
+++ emojillite/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from Sat import cartesianToSpherical, getSatelliteByName
+from math import pi
 
 def index(request):
-    person = "Nigr person"
-    return render(request, "index.html", {'lastname': person})
+    pos, vel = getSatelliteByName("OAO 2")
+    lat, lng, alt = cartesianToSpherical(pos[0], pos[1], pos[2])
+    context = {
+        'lat' : float(lat/pi*180),
+        'lng' : float(lng/pi*180),
+        'alt' : float(alt)
+    }
+    return render(request, "index.html", context)
