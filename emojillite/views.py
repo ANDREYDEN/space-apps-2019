2df1475796f49fb7912457d23dc49c047d6b6ab5
nmyshuniaiev <nmyshuniaiev@gmail.com>
2019-10-19 23:00:36 -0400

    Added multiple satellites support

--- emojillite/views.py
+++ emojillite/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from Satellite_parameters import cartesianToSpherical, getSatelliteByName
 from math import pi
 
+SATELLITES = ['OAO 2', 'NOAA 1', 'NOAA 2 (ITOS-D)']
+
 class Satellite:
-  def __init__(self, lat, lng, alt):
-    self.lat = lat
-    self.lng = lng
-    self.alt = alt
+  def __init__(self, name):
+    pos = getSatelliteByName(name)
+    self.lat, self.lng, self.alt = cartesianToSpherical(pos[0], pos[1], pos[2])
 
   def calc(self):
     self.stats = (str(round(float(self.lat/pi*180), 3)) + " " +
@@ -15,13 +16,5 @@ class Satellite:
     return self.stats
 
 def index(request):
-    pos = getSatelliteByName("OAO 2")
-    lat, lng, alt = cartesianToSpherical(pos[0], pos[1], pos[2])
-    # context = {
-    #     'lat' : round(float(lat/pi*180), 3),
-    #     'lng' : round(float(lng/pi*180), 3),
-    #     'alt' : round(float(alt*1000), 3)
-    # }
-    aoa2 = Satellite(lat, lng, alt)
-    aoa2 = aoa2.calc()
-    return render(request, "index.html", {'aoa2' : aoa2})
+    sats = [Satellite(sat).calc() for sat in SATELLITES]
+    return render(request, "index.html", {'sats': sats})
