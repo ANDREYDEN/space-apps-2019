13005b2b3a38d586b634b4c582c176a195adf145
ANDREYDEN <andrey2850@gmail.com>
2019-10-20 01:24:25 -0400

    Fixed same coordinates

--- emojillite/views.py
+++ emojillite/views.py
@@ -2,19 +2,14 @@ from django.shortcuts import render
 from Satellite_parameters import cartesianToSpherical, getSatelliteByName
 from math import pi
 
-SATELLITES = ['OAO 2', 'NOAA 1', 'NOAA 2 (ITOS-D)']
-
-class Satellite:
-  def __init__(self, name):
-    pos = getSatelliteByName(name)
-    self.lat, self.lng, self.alt = cartesianToSpherical(pos[0], pos[1], pos[2])
-
-  def calc(self):
-    self.stats = (str(round(float(self.lat/pi*180), 3)) + " " +
-                    str(round(float(self.lng/pi*180), 3)) + " " +
-                    str(round(float(self.alt/pi*180), 3)))
-    return self.stats
+SATELLITES = ['OAO 2', 'NOAA 1', 'NOAA 2 (ITOS-D)', 'ISIS 1', 'SERT 2']
 
 def index(request):
-    sats = [Satellite(sat).calc() for sat in SATELLITES]
-    return render(request, "index.html", {'sats': sats})
+    sats = []
+    for name in SATELLITES:
+        lat, lng, alt = getSatelliteByName(name)
+        sats.append((str(round(float(lat/pi*180), 3)) + " " +
+                     str(round(float(lng/pi*180), 3)) + " " +
+                     str(round(float(alt*1000), 3))))
+
+    return render(request, "index.html", {'satellites': sats})
