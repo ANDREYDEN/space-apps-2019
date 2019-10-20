6c062fc351461f7abd135499f086bf491de543bc
ANDREYDEN <andrey2850@gmail.com>
2019-10-19 20:03:44 -0400

    Added .gitignore

--- emojillite/views.py
+++ emojillite/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from Sat import cartesianToSpherical, getSatelliteByName
+from Satellite_parameters import cartesianToSpherical, getSatelliteByName
 from math import pi
 
 def index(request):
-    pos, vel = getSatelliteByName("OAO 2")
+    pos = getSatelliteByName("OAO 2")
     lat, lng, alt = cartesianToSpherical(pos[0], pos[1], pos[2])
     context = {
         'lat' : float(lat/pi*180),
         'lng' : float(lng/pi*180),
-        'alt' : float(alt)
+        'alt' : float(alt*1000)
     }
     return render(request, "index.html", context)
