07f5eead0ea74b1ab1609eb6e3c14aa5fc7707c7
ANDREYDEN <andrey2850@gmail.com>
2019-10-24 11:41:01 -0400

    Heroku testing statics

--- emojillite/settings.py
+++ emojillite/settings.py
@@ -121,6 +121,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'emojillite/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
