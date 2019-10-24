5832c25da6e7314c2f98f76a34dd8b676e5b1eda
ANDREYDEN <andrey2850@gmail.com>
2019-10-24 11:21:50 -0400

    Fixed static files

--- emojillite/settings.py
+++ emojillite/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'c$auxs22n_1m-7l)enwk8ry+eqvl_gpna(9=c$0a=8==x)8w4g'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
     'emojillite.herokuapp.com'
 ]
 
@@ -119,7 +121,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
