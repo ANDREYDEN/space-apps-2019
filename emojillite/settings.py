820d2ac38ef4288dc565cb176feef85fc4c22a4e
ANDREYDEN <andrey2850@gmail.com>
2019-10-24 12:19:23 -0400

    Added statics

--- emojillite/settings.py
+++ emojillite/settings.py
@@ -123,7 +123,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
